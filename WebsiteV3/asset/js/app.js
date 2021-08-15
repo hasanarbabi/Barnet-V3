4e1b419919788b499879993b68162a134072ace3
hasan arbabi <hasanarbabi90@gmail.com>
2021-08-15 17:44:14 +0430

    first commit

--- WebsiteV3/asset/js/app.js
+++ WebsiteV3/asset/js/app.js
@@ -204,6 +204,26 @@ owl.owlCarousel({
     autoplay: true,
     autoplayTimeout: 2000,
     autoplayHoverPause: true,
+    responsive: {
+        0: {
+            items: 1,
+        },
+        600: {
+            items: 1,
+        },
+        900: {
+            items: 1,
+        },
+        1000: {
+            items: 1,
+        },
+        1200: {
+            items: 2,
+        },
+        1700: {
+            items: 3,
+        }
+    }
 });
 
 owl.on('mousewheel', '.owl-stage', function (e) {
